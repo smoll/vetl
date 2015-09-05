76d46b586b4bc171930187ea3050a5fe39c096ba
Shujon Mollah <smollah@theorchard.com>
2015-09-05 00:43:05 -0400

    Add post-ETL test

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 .DEFAULT_GOAL := all
 CURRENT_DIRECTORY := $(shell pwd)
+RSPEC_OPTS := --color --require spec_helper --format documentation --format RspecJunitFormatter --out spec/reports/
 
 all:
 	@make clean
@@ -18,8 +19,9 @@ logs:
 	@docker-compose logs
 
 test:
-	@docker-compose run test rspec spec/pre-etl
+	@rm -rf test/spec/reports
+	@docker-compose run test rspec spec/pre-etl $(RSPEC_OPTS)pre.xml
 	@docker-compose run etl kiba migrate_users.etl
-	# @docker-compose run test rspec spec/post-etl
+	@docker-compose run test rspec spec/post-etl $(RSPEC_OPTS)post.xml
 
 .PHONY: all build clean logs test
---
+++ test/spec/post-etl/etl_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe "etl" do
+  describe "postgres users table" do
+    it "successfully filters out rows with NULL salesRepEmployeeNumber" do
+      # thing = mysql.query("SELECT * FROM classicmodels.customers WHERE customerName = 'Havel & Zbyszek Co'")
+      # byebug
+      customer_to_filter = mysql.query("SELECT * FROM classicmodels.customers WHERE salesRepEmployeeNumber IS NULL").first["customerName"]
+      expect(customer_to_filter.to_s).to_not be_empty
+
+      result = pg.exec("SELECT * FROM users WHERE customerName = 'Havel & Zbyszek Co'").first
+      expect(result).to be_nil
+    end
+  end
+end
