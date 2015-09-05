b970ec0204c93a91a99533f8211ae90590f0ee32
Shujon Mollah <smollah@theorchard.com>
2015-09-04 23:56:12 -0400

    Get rid of non-deterministic sleep

--- test/spec/helpers.rb
+++ test/spec/helpers.rb
@@ -1,10 +1,20 @@
 module Helpers
   def mysql
+    tries ||= 30 # wait up to 30 seconds for db bootstrap to complete
     @mysql ||= Mysql2::Client.new(connect_hash(ENV["MYSQL_URL"]))
+  rescue Mysql2::Error => e
+    sleep 1
+    retry unless (tries -= 1).zero?
+    raise e
   end
 
   def pg
+    tries ||= 5
     @pg ||= PG.connect(ENV["PG_URL"])
+  rescue PG::Error => e
+    sleep 1
+    retry unless (tries -= 1).zero?
+    raise e
   end
 
   private
