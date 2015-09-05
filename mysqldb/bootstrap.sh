6108f7ef78d6058c95b878efbe27b669b56fcff9
Shujon Mollah <smollah@theorchard.com>
2015-09-04 22:51:11 -0400

    Bootstrap DB from inside the mysqldb container

--- mysqldb/bootstrap.sh
+++ mysqldb/bootstrap.sh
@@ -3,13 +3,7 @@
 set -e
 
 # set options
-MYSQL_OPTIONS="-h${MYSQLDB_PORT_3306_TCP_ADDR} -P${MYSQLDB_PORT_3306_TCP_PORT} -uroot -p${MYSQLDB_ENV_MYSQL_ROOT_PASSWORD}"
-
-# check to see if we should skip import
-if [ ! -z ${SKIP_IMPORT} ]
-then
-  exec "$@"
-fi
+MYSQL_OPTIONS="-uroot -p${MYSQL_ROOT_PASSWORD}"
 
 # check to make sure the db is accessible before importing data
 while [ $(mysql --connect-timeout=1 ${MYSQL_OPTIONS} -e "show databases;" > /dev/null 2>&1; echo $?) -ne 0 ]
@@ -24,12 +18,10 @@ echo "Pre import databases:"
 mysql -t ${MYSQL_OPTIONS} -e "show databases;" 2> /dev/null
 
 echo -e "\nImporting all .sql files..."
-cat *.sql | mysql ${MYSQL_OPTIONS} 2> /dev/null
+cat /tmp/sql-bootstrap/*.sql | mysql ${MYSQL_OPTIONS} 2> /dev/null
 echo -e "Import complete"'!'"\n"
 
 echo "Post import databases:"
 mysql -t ${MYSQL_OPTIONS} -e "show databases;" 2> /dev/null
 
 echo -e "\nDatabase bootstrap complete"'!'
-
-exec "$@"
